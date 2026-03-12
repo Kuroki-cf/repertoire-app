0857463eaacc94e0c03f55579e35663bf79c9e5c
Kuroki-cf <mizuhokuroki@icloud.com>
2026-03-12 17:45:30 +0900

    fix: lib/supabaseClient.ts を環境変数参照に変更

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 // 👇 あなたのSupabaseのURLとAPIキーを貼り付けます
-const supabaseUrl = "https://eqmvlcsgenjidbqqshxr.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxbXZsY3NnZW5qaWRicXFzaHhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NDM4OTQsImV4cCI6MjA4ODUxOTg5NH0.Exud8W0VErb0PxQ1perkYD_BjqUHSRA0In4MCFmEaCs";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
